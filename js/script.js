33bf90a1c3b525a330bf9552aea28f06bbb7e03d
Evgenik <109359580+Jeko26@users.noreply.github.com>
2022-08-09 15:41:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@ burger.addEventListener('click', function () {
 	links.classList.toggle('burger--open');
 	burger.classList.toggle('burger--cross');
 });
+/* слайдер */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
